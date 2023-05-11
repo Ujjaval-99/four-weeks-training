aa4cd3fed1153d56b58a04f406442e63b3175928
Ujjaval-99 <ujjavalmistry99@gmail.com>
2023-05-11 17:57:34 +0530

    new

--- week-1/day-4/exercise-6/LinqFilterAndSort/Program.cs
+++ week-1/day-4/exercise-6/LinqFilterAndSort/Program.cs
@@ -15,23 +15,6 @@ class Program
     {
 
         List<Person> people = new List<Person>
-        {
-            new Person { FirstName = "Rahul", LastName = "Mishra", Age = 20 },
-            new Person { FirstName = "Janki", LastName = "Sharma", Age = 35 },
-            new Person { FirstName = "Minesh", LastName = "Mishra", Age = 30 },
-            new Person { FirstName = "Mayank", LastName = "Sharma", Age = 25 },
-
-        };
-
-
-        var filteredPeople = people.Where(p => p.Age >= 25)
-                                   .OrderBy(p => p.LastName)
-                                   .ThenBy(p => p.FirstName);
-
-
-        foreach (var person in filteredPeople)
-        {
-            Console.WriteLine($"Name: {person.FirstName} {person.LastName}, Age: {person.Age}");
-        }
+       
     }
 }
