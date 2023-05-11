8d4b845d24e35b995249836e71ad839a78e3f2b9
Ujjaval-99 <ujjavalmistry99@gmail.com>
2023-05-11 14:56:06 +0530

    LINQ to XML

--- week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
+++ week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
@@ -13,7 +13,7 @@ class Program
 {
     static void Main(string[] args)
     {
-  
+
         List<Person> people = new List<Person>()
         {
             new Person { Name = "John", Age = 25, Country = "USA" },
@@ -23,11 +23,11 @@ class Program
             new Person { Name = "Daniel", Age = 32, Country = "USA" }
         };
 
-    
+
         int filterAge = 30;
         var filteredPeople = people.Where(p => p.Age > filterAge);
 
-     
+
         var sortedPeople = filteredPeople.OrderBy(p => p.Name);
 
         var projectedPeople = sortedPeople.Select(p => new { p.Name, p.Country });
