12f5d68807d33a293c0919df32e5b7dbb774d0f5
Ujjaval-99 <ujjavalmistry99@gmail.com>
2023-05-11 16:50:02 +0530

    Linq-FilterAndSort

--- week-1/day-4/exercise-6/LinqFilterAndSort/Program.cs
+++ week-1/day-4/exercise-6/LinqFilterAndSort/Program.cs
@@ -1,12 +1,37 @@
-﻿namespace LinqFilterAndSort
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Person
 {
-    internal class Program
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+        List<Person> people = new List<Person>
         {
-            // Create a list of Person objects
-            // Use LINQ to filter and sort the list
-            // Print the filtered and sorted list of people to the console
+            new Person { FirstName = "Rahul", LastName = "Mishra", Age = 20 },
+            new Person { FirstName = "Janki", LastName = "Sharma", Age = 35 },
+            new Person { FirstName = "Minesh", LastName = "Mishra", Age = 30 },
+            new Person { FirstName = "Mayank", LastName = "Sharma", Age = 25 },
+
+        };
+
+
+        var filteredPeople = people.Where(p => p.Age >= 25)
+                                   .OrderBy(p => p.LastName)
+                                   .ThenBy(p => p.FirstName);
+
+
+        foreach (var person in filteredPeople)
+        {
+            Console.WriteLine($"Name: {person.FirstName} {person.LastName}, Age: {person.Age}");
         }
     }
 }
