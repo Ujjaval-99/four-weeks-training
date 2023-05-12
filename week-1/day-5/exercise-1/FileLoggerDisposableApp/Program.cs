9c1eb1a9b3614f60d9b0dbc2300ddd8f304bf4b2
Ujjaval-99 <ujjavalmistry99@gmail.com>
2023-05-12 13:04:03 +0530

    new

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -4,26 +4,32 @@
     {
         static void Main(string[] args)
         {
+            using (var logger = new FileLogger("Ujjval Mistry.txt"))
+            {
+                logger.Log("I am Ujjval...!");
+                // some code here
+                logger.Log("I'm a unemployeed.....!!");
+                logger.Dispose();
+            }
             // Use FileLogger and dispose of it properly
         }
     }
-
     class FileLogger : IDisposable
     {
         private StreamWriter _writer;
-
         public FileLogger(string filePath)
         {
+            _writer = new StreamWriter(filePath);
             // Initialize StreamWriter instance
         }
-
         public void Dispose()
         {
+            _writer.Dispose();
             // Implement IDisposable pattern
         }
-
         public void Log(string message)
         {
+            _writer?.WriteLine(message);
             // Write message to the file
         }
     }
